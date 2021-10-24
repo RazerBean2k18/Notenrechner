2d383f8832371f1cace5979be080ca78b919245f
Colin Helmer <colin.helmer@t-online.de>
2021-10-24 22:29:40 +0200

    Changes to database structure

--- database-initialisation.py
+++ database-initialisation.py
@@ -4,4 +4,23 @@ conn = sql.connect('MasterDataBase.db')
 
 c = conn.cursor()
 
+c.execute("""
+CREATE TABLE IF NOT EXISTS subjects (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    written INTEGER
+)
+""")
+
+c.execute("""
+CREATE TABLE IF NOT EXISTS grades (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    subject_name TEXT NOT NULL,
+    grade INTEGER,
+    FOREIGN KEY (subject_name) REFERENCES subjects (name)
+)
+""")
+
+conn.commit()
+
 conn.close()
