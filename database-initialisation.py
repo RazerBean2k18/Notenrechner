a5c3aab9cec8d421e0a7263d55a31a92741c88cb
Colin Helmer <colin.helmer@t-online.de>
2021-10-25 14:38:33 +0200

    Finish the database-initialisation.py

--- database-initialisation.py
+++ database-initialisation.py
@@ -8,7 +8,10 @@ c.execute("""
 CREATE TABLE IF NOT EXISTS subjects (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
-    written INTEGER
+    written_weight INTEGER,
+    oral_weight INTEGER,
+    FOREIGN KEY (subject_written_weight) REFERENCES weighting (value),
+    FOREIGN KEY (subject_oral_weight) REFERENCES weighting (value)
 )
 """)
 
@@ -17,7 +20,24 @@ CREATE TABLE IF NOT EXISTS grades (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     subject_name TEXT NOT NULL,
     grade INTEGER,
-    FOREIGN KEY (subject_name) REFERENCES subjects (name)
+    weightng_type TEXT NOT NULL,
+    semester TEXT,
+    FOREIGN KEY (subject_name) REFERENCES subjects (name),
+    FOREIGN KEY (semester) REFERENCES semesters (name)
+)
+""")
+
+c.execute("""
+CREATE TABLE IF NOT EXISTS semesters (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT
+)
+""")
+
+c.execute("""
+CREATE TABLE IF NOT EXISTS weighting (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    value INTEGER
 )
 """)
 
